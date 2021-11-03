7c56b8e19b9ac991c7c40356647fce02c5d3f3fb
LandaMeen <princ228321@gmail.com>
2021-11-03 16:38:45 +0300

    Added glfw library

---
+++ build/CMakeFiles/MyGame.dir/src/programm.cpp.obj.d
@@ -0,0 +1,9 @@
+CMakeFiles/MyGame.dir/src/programm.cpp.obj: \
+ D:\MyGameCpp\MyGame\src\programm.cpp \
+ D:/MyGameCpp/MyGame/external/glfw/include/GLFW/glfw3.h \
+ d:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ d:\mingw\lib\gcc\mingw32\9.2.0\include\stdint.h \
+ d:\mingw\include\stdint.h d:\mingw\include\_mingw.h \
+ d:\mingw\include\msvcrtver.h d:\mingw\include\w32api.h \
+ d:\mingw\include\sdkddkver.h d:\mingw\include\features.h \
+ d:\mingw\include\gl\gl.h
---
+++ build/external/glfw/src/CMakeFiles/glfw.dir/wgl_context.c.obj.d
@@ -0,0 +1,39 @@
+external/glfw/src/CMakeFiles/glfw.dir/wgl_context.c.obj: \
+ D:\MyGameCpp\MyGame\external\glfw\src\wgl_context.c \
+ D:\MyGameCpp\MyGame\external\glfw\src\internal.h \
+ d:\mygamecpp\mygame\external\glfw\include\glfw\glfw3.h \
+ d:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ d:\mingw\lib\gcc\mingw32\9.2.0\include\stdint.h \
+ d:\mingw\include\stdint.h d:\mingw\include\_mingw.h \
+ d:\mingw\include\msvcrtver.h d:\mingw\include\w32api.h \
+ d:\mingw\include\sdkddkver.h d:\mingw\include\features.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\platform.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\null_platform.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\win32_platform.h \
+ d:\mingw\include\wctype.h d:\mingw\include\windows.h \
+ d:\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h \
+ d:\mingw\include\windef.h d:\mingw\include\winnt.h \
+ d:\mingw\include\winerror.h d:\mingw\include\string.h \
+ d:\mingw\include\basetsd.h d:\mingw\include\pshpack4.h \
+ d:\mingw\include\poppack.h d:\mingw\include\wincon.h \
+ d:\mingw\include\winbase.h d:\mingw\include\wingdi.h \
+ d:\mingw\include\winuser.h d:\mingw\include\dbt.h \
+ d:\mingw\include\winnls.h d:\mingw\include\winver.h \
+ d:\mingw\include\winnetwk.h d:\mingw\include\winreg.h \
+ d:\mingw\include\reason.h d:\mingw\include\winsvc.h \
+ D:/MyGameCpp/MyGame/external/glfw/deps/mingw/dinput.h \
+ d:\mingw\include\objbase.h d:\mingw\include\rpc.h \
+ d:\mingw\include\rpcdce.h d:\mingw\include\basetyps.h \
+ d:\mingw\include\rpcdcep.h d:\mingw\include\rpcnsi.h \
+ d:\mingw\include\rpcnterr.h d:\mingw\include\rpcndr.h \
+ d:\mingw\include\rpcnsip.h d:\mingw\include\objfwd.h \
+ d:\mingw\include\stdlib.h d:\mingw\include\errno.h \
+ d:\mingw\include\wtypes.h d:\mingw\include\unknwn.h \
+ d:\mingw\include\objidl.h d:\mingw\include\cguid.h \
+ D:/MyGameCpp/MyGame/external/glfw/deps/mingw/_mingw_dxhelper.h \
+ D:/MyGameCpp/MyGame/external/glfw/deps/mingw/xinput.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\null_joystick.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\win32_joystick.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\win32_thread.h \
+ D:\MyGameCpp\MyGame\external\glfw\src\win32_time.h \
+ d:\mingw\include\assert.h
